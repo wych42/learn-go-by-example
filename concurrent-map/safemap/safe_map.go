7f2403c60a8e61b919ad30ce0cf0bb6bf561df79
wych42 <wych101010@gmail.com>
2021-04-07 00:04:06 +0800

    update

--- concurrent-map/safemap/safe_map.go
+++ concurrent-map/safemap/safe_map.go
@@ -4,13 +4,13 @@ import "sync"
 
 type SafeMap struct {
 	data map[interface{}]interface{}
-	mu   *sync.RWMutex
+	mu   sync.RWMutex
 }
 
 func NewSafeMap() *SafeMap {
 	return &SafeMap{
 		data: make(map[interface{}]interface{}),
-		mu:   &sync.RWMutex{},
+		mu:   sync.RWMutex{},
 	}
 }
 
@@ -21,15 +21,16 @@ func (m *SafeMap) Len() int {
 }
 
 // Put: return is key exists
-func (m *SafeMap) Put(key, value interface{}) bool {
+func (m *SafeMap) Store(key, value interface{}) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
-	_, ok := m.data[key]
+	if m.data == nil {
+		m.data = make(map[interface{}]interface{})
+	}
 	m.data[key] = value
-	return ok
 }
 
-func (m *SafeMap) Get(key interface{}) (interface{}, bool) {
+func (m *SafeMap) Load(key interface{}) (interface{}, bool) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
 
@@ -37,13 +38,49 @@ func (m *SafeMap) Get(key interface{}) (interface{}, bool) {
 	return value, ok
 }
 
-// Delete: return is key exists
-func (m *SafeMap) Delete(key interface{}) bool {
+func (m *SafeMap) LoadOrStore(key, value interface{}) (actual interface{}, loaded bool) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	actual, loaded = m.data[key]
+	if !loaded {
+		if m.data == nil {
+			m.data = make(map[interface{}]interface{})
+		}
+		m.data[key] = value
+		actual = value
+	}
+	return
+}
+
+func (m *SafeMap) Delete(key interface{}) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	_, ok := m.data[key]
 	if ok {
 		delete(m.data, key)
 	}
-	return ok
+	return
+}
+
+// Delete: return is key exists
+func (m *SafeMap) LoadAndDelete(key interface{}) (value interface{}, loaded bool) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	value, loaded = m.data[key]
+	if loaded {
+		delete(m.data, key)
+	}
+	return
+}
+
+func (m *SafeMap) Range(f func(key, value interface{}) (shouldContinue bool)) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	for k, v := range m.data {
+		ok := f(k, v)
+		if !ok {
+			return
+		}
+	}
 }
--- concurrent-map/safemap/safe_map_test.go
+++ concurrent-map/safemap/safe_map_test.go
@@ -1,28 +1,117 @@
 package safemap
 
 import (
+	"fmt"
+	"reflect"
 	"sync"
+	"sync/atomic"
 	"testing"
 )
 
-const hits = 16
+// copy from https://github.com/golang/go/blob/b345a306a0258085b65081cf2dadc238dc7e26ee/src/sync/map_reference_test.go#L15
+type mapInterface interface {
+	Load(interface{}) (interface{}, bool)
+	Store(key, value interface{})
+	LoadOrStore(key, value interface{}) (actual interface{}, loaded bool)
+	LoadAndDelete(key interface{}) (value interface{}, loaded bool)
+	Delete(interface{})
+	Range(func(key, value interface{}) (shouldContinue bool))
+}
 
-func BenchmarkSafeMapMostlyHits(b *testing.B) {
-	safeMap := NewSafeMap()
-	for i := 0; i < hits; i++ {
-		safeMap.Put(i, i)
-	}
-	for i := 0; i < hits*100; i++ {
-		safeMap.Get(i % hits)
+type bench struct {
+	setup func(*testing.B, mapInterface)
+	perG  func(b *testing.B, pb *testing.PB, i int, m mapInterface)
+}
+
+func benchMap(b *testing.B, bench bench) {
+	for _, m := range [...]mapInterface{&SafeMap{}, &sync.Map{}} {
+		b.Run(fmt.Sprintf("%T", m), func(b *testing.B) {
+			m = reflect.New(reflect.TypeOf(m).Elem()).Interface().(mapInterface)
+			if bench.setup != nil {
+				bench.setup(b, m)
+			}
+
+			b.ResetTimer()
+
+			var i int64
+			b.RunParallel(func(pb *testing.PB) {
+				id := int(atomic.AddInt64(&i, 1) - 1)
+				bench.perG(b, pb, id*b.N, m)
+			})
+		})
 	}
 }
 
-func BenchmarkSyncMapMostlyHits(b *testing.B) {
-	syncMap := sync.Map{}
-	for i := 0; i < hits; i++ {
-		syncMap.Store(i, i)
-	}
-	for i := 0; i < hits*100; i++ {
-		syncMap.Load(i % hits)
-	}
+func BenchmarkLoadMostlyHits(b *testing.B) {
+	const hits, misses = 1023, 1
+
+	benchMap(b, bench{
+		setup: func(_ *testing.B, m mapInterface) {
+			for i := 0; i < hits; i++ {
+				m.LoadOrStore(i, i)
+			}
+			// Prime the map to get it into a steady state.
+			for i := 0; i < hits*2; i++ {
+				m.Load(i % hits)
+			}
+		},
+
+		perG: func(b *testing.B, pb *testing.PB, i int, m mapInterface) {
+			for ; pb.Next(); i++ {
+				m.Load(i % (hits + misses))
+			}
+		},
+	})
+}
+func BenchmarkLoadMostlyMisses(b *testing.B) {
+	const hits, misses = 1, 1023
+
+	benchMap(b, bench{
+		setup: func(_ *testing.B, m mapInterface) {
+			for i := 0; i < hits; i++ {
+				m.LoadOrStore(i, i)
+			}
+			// Prime the map to get it into a steady state.
+			for i := 0; i < hits*2; i++ {
+				m.Load(i % hits)
+			}
+		},
+
+		perG: func(b *testing.B, pb *testing.PB, i int, m mapInterface) {
+			for ; pb.Next(); i++ {
+				m.Load(i % (hits + misses))
+			}
+		},
+	})
+}
+
+func BenchmarkLoadOrStoreBalanced(b *testing.B) {
+	const hits, misses = 128, 128
+
+	benchMap(b, bench{
+		setup: func(b *testing.B, m mapInterface) {
+			for i := 0; i < hits; i++ {
+				m.LoadOrStore(i, i)
+			}
+			// Prime the map to get it into a steady state.
+			for i := 0; i < hits*2; i++ {
+				m.Load(i % hits)
+			}
+		},
+
+		perG: func(b *testing.B, pb *testing.PB, i int, m mapInterface) {
+			for ; pb.Next(); i++ {
+				j := i % (hits + misses)
+				if j < hits {
+					if _, ok := m.LoadOrStore(j, i); !ok {
+						b.Fatalf("unexpected miss for %v", j)
+					}
+				} else {
+					if v, loaded := m.LoadOrStore(i, i); loaded {
+						b.Fatalf("failed to store %v: existing value %v", i, v)
+					}
+				}
+			}
+		},
+	})
 }
