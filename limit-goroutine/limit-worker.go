f983bdbb3b169c680cb3d561b4ac666bdff38567
wych42 <wych101010@gmail.com>
2021-05-23 21:21:08 +0800

    add limit goroutine

---
+++ limit-goroutine/limit-worker.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(id int) {
+	fmt.Printf("working on job %d \n", id)
+	time.Sleep(2 * time.Second)
+}
+
+// 限制并发数量
+var limit = make(chan struct{}, 2)
+
+// 等任务完成
+var wait = make(chan int)
+
+func main() {
+	for i := 0; i < 5; i++ {
+		limit <- struct{}{}
+		go func(id int) {
+			fmt.Printf("start job %d\n", id)
+			// 尝试写 channel，能写进去就可以执行
+			worker(id)
+			// 释放
+			<-limit
+			wait <- id
+		}(i)
+	}
+	for i := 0; i < 5; i++ {
+		id := <-wait
+		fmt.Printf("finish job %d\n", id)
+	}
+}
